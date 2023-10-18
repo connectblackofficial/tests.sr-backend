cebbd80c3a3036b0e083c95ebde977295c1a9d94
Gabriel Lima <gabriel@gabriellima.com>
2023-10-18 14:45:40 -0300

    feat(middlewares): add protection to xss and error handler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,20 @@
+import type { Request, Response } from 'express';
+import { StatusCodes, getReasonPhrase } from 'http-status-codes';
+import { fails } from '../helpers/response.helper';
+
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response
+): void => {
+  fails(res, {
+    status: StatusCodes.INTERNAL_SERVER_ERROR, 
+    errors: [
+      {
+        code: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+        field: 'server',
+        message: err.message
+      }
+    ]
+  });
+};
