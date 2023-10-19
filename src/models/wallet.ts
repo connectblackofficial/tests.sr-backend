4c01d7f2f4d0f73b1747550e6947946680c5d9f5
Gabriel Lima <gabriel@gabriellima.com>
2023-10-19 01:56:18 -0300

    feat(wallet): add entity

--- src/models/wallet.ts
+++ src/models/wallet.ts
@@ -1,10 +1,13 @@
 import * as yup from 'yup';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 export interface WalletRequestBody {
   userId: string;
   balance: number;
   walletName: string;
   timestamp?: string;
+  createdAt?: Date;
+  syncedAt?: Date;
 }
 
 export const walletRequestSchema = yup.object({
@@ -14,3 +17,39 @@ export const walletRequestSchema = yup.object({
     walletName: yup.string().required()
   })
 });
+
+@Entity({ name: 'wallets' })
+export class Wallet {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id!: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    name: 'user_id'
+  })
+  userId!: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    name: 'wallet_name'
+  })
+  walletName!: string;
+
+  @Column({
+    type: 'decimal',
+    precision: 8,
+    scale: 2
+  })
+  balance!: number;
+
+  @Column()
+  reference!: string;
+
+  @Column({ name: 'created_at' })
+  createdAt!: Date;
+
+  @Column({ name: 'synced_at' })
+  syncedAt!: Date;
+}
