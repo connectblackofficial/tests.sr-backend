4a81639f0467837e3069582396d2ba5ff8a7e96d
Gabriel Lima <gabriel@gabriellima.com>
2023-10-18 21:10:41 -0300

    fix(config): config and add libs

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: `../../.env.${process.env.NODE_ENV}` });
+dotenv.config({ path: `.env.${process.env.NODE_ENV || 'local'}` });
 
 export const PORT = Number(process.env.PORT) || 3000;
 export const NODE_ENV = String(process.env.NODE_ENV);
+export const REDIS_HOST = String(process.env.REDIS_HOST);
+export const REDIS_PORT = Number(process.env.REDIS_PORT) || 6379;
+export const REDIS_PASSWORD = String(process.env.REDIS_PASSWORD);
