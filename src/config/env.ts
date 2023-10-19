094c73fcdcb693a0534652141a0bb3ff1c943f70
Gabriel Lima <gabriel@gabriellima.com>
2023-10-19 01:55:18 -0300

    feat(structure): configure database

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,3 +6,10 @@ export const NODE_ENV = String(process.env.NODE_ENV);
 export const REDIS_HOST = String(process.env.REDIS_HOST);
 export const REDIS_PORT = Number(process.env.REDIS_PORT) || 6379;
 export const REDIS_PASSWORD = String(process.env.REDIS_PASSWORD);
+export const SYNC_ITEMS_PER_CYCLE = Number(process.env.SYNC_ITEMS_PER_CYCLE);
+
+export const MYSQL_HOST = String(process.env.MYSQL_HOST);
+export const MYSQL_PORT = Number(process.env.MYSQL_PORT);
+export const MYSQL_DATABASE = String(process.env.MYSQL_DATABASE);
+export const MYSQL_USER = String(process.env.MYSQL_USER);
+export const MYSQL_PASSWORD = String(process.env.MYSQL_PASSWORD);
