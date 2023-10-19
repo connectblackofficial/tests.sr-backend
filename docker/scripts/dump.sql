4445da8fd5ef68bdb7a39ec0e3954b15c1895851
Gabriel Lima <gabriel@gabriellima.com>
2023-10-19 01:54:57 -0300

    feat(structure): add libs and configure database

--- docker/scripts/dump.sql
+++ docker/scripts/dump.sql
@@ -5,3 +5,14 @@ SET collation_connection = utf8_general_ci;
 
 CREATE DATABASE IF NOT EXISTS wallet_manager;
 USE wallet_manager;
+
+CREATE TABLE IF NOT EXISTS `wallets` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `balance` decimal(8,2) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `wallet_name` varchar(255) NOT NULL,
+  `reference` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `synced_at` datetime NOT NULL,
+  CONSTRAINT `id` PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
